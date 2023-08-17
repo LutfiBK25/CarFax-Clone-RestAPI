89d25d89f4ada95ced60d02372effeabb65316da
LutfiBK25 <122565135+LutfiBK25@users.noreply.github.com>
2023-08-16 23:51:36 -0400

    Created the project

---
+++ src/test/java/com/lutfibk25/RestApiDemo/RestApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lutfibk25.RestApiDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
